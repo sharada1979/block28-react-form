d50a7b5b1e77b414c585f3c1ee2a4d70dd28161d
sharada <sharadasarmah1@gmail.com>
2023-08-04 22:24:02 -0400

    finish homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { Route, Routes } from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+function App(){
+    const [count, setCount] = useState(0)
+    return (
+<div id="container">
+<Navbar />
+<div id="navbar">{/* navigation here */}</div>
+<div id="main-section">
+    <Routes>
+    <Route path='/' element={<Home />} /> 
+        <Route path='/blue' element={<Blue />} />
+        <Route path='/red' element={ <Red />} />
+    </Routes>
+</div>
+</div>
+    )
 }
-
 export default App
